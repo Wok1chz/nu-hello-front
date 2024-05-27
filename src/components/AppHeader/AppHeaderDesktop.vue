5212407910fb21c617f0c031d25850f3a501fad3
Wok1chz <face2x2x2@gmail.com>
2024-05-27 13:24:04 +0300

    Создал хедер

--- src/components/AppHeader/AppHeaderDesktop.vue
+++ src/components/AppHeader/AppHeaderDesktop.vue
@@ -5,15 +5,27 @@
     <div class="header">
         <div class="container">
             <div class="header-content">
-                <span> LOGO </span>
-                <AppHeaderProfile />
+                <img 
+                src="@/assets/svg/logo.svg"
+                />
+                <AppHeaderProfile
+                />
             </div>
         </div>
     </div>
 </template>
 <style scoped lang="scss">
+@import '@/assets/css/common';
     .header-content {
+        min-height: 48px;
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #D0D0D0;
+    }
+    img {
+        width: 48px;
+        height: 48px;
     }
 </style>
--- src/components/AppHeader/AppHeaderProfile.vue
+++ src/components/AppHeader/AppHeaderProfile.vue
@@ -2,10 +2,46 @@
     import { useCommonStore } from '~/store/common';
 
     const commonStore = useCommonStore();
+    const userName = 'Ахмед';
 </script>
 <template>
-    <div>
-        <span v-if="1==1">PROFILE</span>
-        <span v-else>PHOTO</span>
+    <div class="profile">
+        <div class="profile__circle"> 
+            <img 
+            class="profile__circle__img"
+            src="@/assets/tmp/misha.jpg"
+            />    
+        </div>
+        {{ userName }}
     </div>
 </template>
+<style scoped lang="scss">
+    .profile {
+        display: flex;
+        align-items: center;
+        
+        &__circle {
+            border: 2px solid #ae86f9; 
+            background-color: #D0D0D0;
+            justify-content: center;
+            min-width: 36px; 
+            min-height: 36px; 
+            border-radius: 50%; 
+            margin: 0px 10px; 
+            display: flex; 
+            vertical-align: top;
+
+            &__img {
+                border-radius: 50%; 
+                width: 36px;
+                height: 36px;
+            }
+        }
+
+        &__modal-arrow {
+        padding-left: 4px;
+        width: 8px;
+        height: 8px;
+    }
+    }
+</style>
