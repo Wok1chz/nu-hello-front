a872b74bc6d787f36d8f8b03139b1a34449b50bf
Wok1chz <face2x2x2@gmail.com>
2024-06-02 21:22:49 +0300

    Добавил дефолтный шаблон для инпутов, наброски верстки авторизации /auth

--- src/components/AppAuth/AppAuth.vue
+++ src/components/AppAuth/AppAuth.vue
@@ -1,12 +1,28 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
     const email = ref('');
+    const password = ref('');
+
 </script>
 <template>
     <div class="index">
-        <CommonInput 
-            v-model="email"
-        />
+        <div>
+            <img
+                class="index__img" 
+                src="@/assets/images/auth.jpg"
+            />
+        </div>
+        <div class="index__input">
+            <CommonInput 
+                v-model="email"
+                :title="'Логин'"
+            />
+            <CommonInput 
+                v-model="password"
+                :title="'Пароль'"
+            />
+
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
@@ -15,7 +31,20 @@
         box-sizing: border-box;
         margin: 0 auto;
         display: flex;
-        justify-content: space-around;
+        justify-content: center;
+        column-gap: 16px;
         width: 100%;
+        align-items: center;
+
+        &__img {
+            width: 600px;
+            height: 600px;
+        }
+
+        &__input {
+            display: flex;
+            flex-direction: column;
+            row-gap: 12px
+        }
     }
 </style>
--- src/components/common/Input.vue
+++ src/components/common/Input.vue
@@ -1,12 +1,61 @@
-<script lang="ts" setup>
-    const modelValue = defineModel()
+<script setup lang="ts">
+    const modelValue = defineModel();
+    defineProps({
+            title: String
+        });
 
 </script>
 <template>
-    <div>
-        {{ modelValue }}
-        <input 
+    <div 
+        :class="['input', {
+            'without-title:': !title
+        }]"
+    >
+        <div class="input_wrapper">
+            <label 
+                v-if="title"
+                :class="['input__title']"
+            >
+                {{ title }}
+            </label>
+            <input 
             v-model="modelValue"
-        />
+            class="input__input"
+        />            
+        </div>    
+
     </div>
 </template>
+<style scoped lang="scss">
+    .input {
+        position: relative;
+
+        &__wrapper {
+            position: relative;
+        }
+
+        &__title {
+            font-size: 12px;
+            line-height: 16px;
+            color: #D94C40;
+            margin: 0;
+            position: absolute;
+            top: 5px;
+            left: 16px;
+        }
+
+        &__input {
+            font-size: 16px;
+            color: black;
+            background-color: #FBF7DE;
+            line-height: 22px;
+            border-radius: 8px;
+            padding: 21px 40px 5px 16px;
+            border:none;
+            cursor: pointer;
+            outline: none;
+            box-sizing: border-box;
+            border: 1px solid transparent;
+        }
+    }
+</style>
